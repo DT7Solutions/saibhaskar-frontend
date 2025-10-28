c605347524971e8ea791a9d11e7269eb87b4a530
Hemanthallam <hemanthallam009@gmail.com>
2025-10-28 15:14:17 +0530

    domain changed

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 const API_CONFIG = {
-    BASE_URL: 'https://saibhaskarhospital.com',
+    BASE_URL: 'https://api.saibhaskarhospital.com',
     ENDPOINTS: {
         LOGIN: '/api/login/',
         LOGOUT: '/api/logout/',
